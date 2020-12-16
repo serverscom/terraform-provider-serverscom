e72e2e3b793f7bbd1dff02120554d75ba8a0252b
Kirill Zaitsev <kirik910@gmail.com>
2020-12-16 12:51:38 +0200

    Update GoReleaser

--- main.go
+++ main.go
@@ -5,6 +5,11 @@ import (
 	"github.com/serverscom/terraform-provider-serverscom/serverscom"
 )
 
+var (
+	version string = "dev"
+	commit  string = ""
+)
+
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: serverscom.Provider})
